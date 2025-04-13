34317fe1aeae774968f72d94bef70fd6d84e2772
ChampSoham <soham.v.sitapara@gmail.com>
2025-04-13 16:42:37 +0530

    configuration commit

---
+++ src/client/config.ts
@@ -0,0 +1,5 @@
+export const CLIENT_CONFIG = {
+    SERVER_URL: 'https://192.168.29.202:3000',  // Replace with server laptop's IP
+    AUTH_URL: 'https://192.168.29.180:3001',    // Replace with authenticator laptop's IP
+    POLL_INTERVAL: 5000  // Poll for updates every 5 seconds
+}; 
